45dcd6125e067fbbd2c54893fdb3acc1bf37d20d
Mathias L. Baumann <mathias.baumann@frequenz.com>
2024-02-06 11:00:48 +0100

    Delete cookiecutter template leftovers
    Signed-off-by: Mathias L. Baumann <mathias.baumann@frequenz.com>

--- tests/test_client_common.py
+++ tests/test_client_common.py
@@ -2,17 +2,3 @@
 # Copyright © 2023 Frequenz Energy-as-a-Service GmbH
 
 """Tests for the frequenz.client.common package."""
-import pytest
-
-from frequenz.client.common import delete_me
-
-
-def test_client_common_succeeds() -> None:  # TODO(cookiecutter): Remove
-    """Test that the delete_me function succeeds."""
-    assert delete_me() is True
-
-
-def test_client_common_fails() -> None:  # TODO(cookiecutter): Remove
-    """Test that the delete_me function fails."""
-    with pytest.raises(RuntimeError, match="This function should be removed!"):
-        delete_me(blow_up=True)
