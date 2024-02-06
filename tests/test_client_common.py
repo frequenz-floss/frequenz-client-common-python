d62dfa446807be00556314f7f17aab167868b602
Mathias L. Baumann <mathias.baumann@frequenz.com>
2024-02-06 11:32:24 +0100

    Add components types from SDK
    Signed-off-by: Mathias L. Baumann <mathias.baumann@frequenz.com>

--- tests/test_client_common.py
+++ tests/test_client_common.py
@@ -2,3 +2,11 @@
 # Copyright © 2023 Frequenz Energy-as-a-Service GmbH
 
 """Tests for the frequenz.client.common package."""
+
+from frequenz.client.common.microgrid.components import ComponentCategory
+
+
+def test_components() -> None:
+    """Test the components."""
+    for category in ComponentCategory:
+        assert ComponentCategory.from_proto(category.to_proto()) == category
