887bbfc3a1d34be2929b4de03b75e383da352309
Leandro Lucarella <luca-frequenz@llucax.com>
2025-08-18 17:25:11 +0200

    Add streaming.Event bindings to the top-level
    This can be added in a backwards-compatible way, so we can add it to the
    top-level.
    
    Signed-off-by: Leandro Lucarella <luca-frequenz@llucax.com>

--- tests/test_streaming.py
+++ tests/test_streaming.py
@@ -1,10 +1,10 @@
 # License: MIT
 # Copyright © 2025 Frequenz Energy-as-a-Service GmbH
 
-"""Tests for the frequenz.client.common.v1alpha8.streaming package."""
+"""Tests for the frequenz.client.common.streaming package."""
 
 from frequenz.client.common.enum_proto import enum_from_proto
-from frequenz.client.common.v1alpha8.streaming import Event
+from frequenz.client.common.streaming import Event
 
 
 def test_event_enum() -> None:
