cc86024e197691f74549908a4a3938e0843dbca9
Leandro Lucarella <luca-frequenz@llucax.com>
2026-06-01 16:37:57 +0200

    Export new symbols
    Signed-off-by: Leandro Lucarella <luca-frequenz@llucax.com>

---
+++ src/frequenz/client/common/grid/__init__.py
@@ -0,0 +1,11 @@
+# License: MIT
+# Copyright © 2025 Frequenz Energy-as-a-Service GmbH
+
+"""Grid definitions for the energy market."""
+
+from ._delivery_area import DeliveryArea, EnergyMarketCodeType
+
+__all__ = [
+    "DeliveryArea",
+    "EnergyMarketCodeType",
+]
