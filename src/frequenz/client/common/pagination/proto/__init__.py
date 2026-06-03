a8c5b8a1984e5a9f722e64d5f290e9a668ada2e6
Leandro Lucarella <luca-frequenz@llucax.com>
2026-06-03 21:18:21 +0000

    Add `pagination` v1alpha8 conversion functions
    Signed-off-by: Leandro Lucarella <luca-frequenz@llucax.com>

---
+++ src/frequenz/client/common/pagination/proto/__init__.py
@@ -0,0 +1,4 @@
+# License: MIT
+# Copyright © 2026 Frequenz Energy-as-a-Service GmbH
+
+"""Pagination objects from/to proto conversion functions."""
---
+++ src/frequenz/client/common/pagination/proto/v1alpha8/_pagination_info.py
@@ -0,0 +1,40 @@
+# License: MIT
+# Copyright © 2026 Frequenz Energy-as-a-Service GmbH
+
+"""Conversion of PaginationInfo from/to protobuf v1alpha8."""
+
+from frequenz.api.common.v1alpha8.pagination.pagination_info_pb2 import (
+    PaginationInfo as PaginationInfoPb,
+)
+
+from ....pagination import PaginationInfo
+
+
+def pagination_info_from_proto(message: PaginationInfoPb) -> PaginationInfo:
+    """Convert a protobuf PaginationInfo to a PaginationInfo object.
+
+    Args:
+        message: The protobuf message to convert.
+
+    Returns:
+        The resulting PaginationInfo object.
+    """
+    return PaginationInfo(
+        total_items=message.total_items,
+        next_page_token=message.next_page_token if message.next_page_token else None,
+    )
+
+
+def pagination_info_to_proto(info: PaginationInfo) -> PaginationInfoPb:
+    """Convert a PaginationInfo object to a protobuf PaginationInfo.
+
+    Args:
+        info: The PaginationInfo object to convert.
+
+    Returns:
+        The resulting protobuf message.
+    """
+    return PaginationInfoPb(
+        total_items=info.total_items,
+        next_page_token=info.next_page_token,
+    )
