0dbe96952f26cf429e78109ca4b88e3176e43a1e
Leandro Lucarella <luca-frequenz@llucax.com>
2026-06-03 21:12:16 +0000

    Move `Event` enum to its own `_event.py` module
    Signed-off-by: Leandro Lucarella <luca-frequenz@llucax.com>

--- src/frequenz/client/common/streaming/__init__.py
+++ src/frequenz/client/common/streaming/__init__.py
@@ -3,20 +3,6 @@
 
 """Type wrappers for the generated protobuf messages."""
 
-from enum import Enum
+from ._event import Event
 
-
-class Event(Enum):
-    """Enum representing the type of streaming event."""
-
-    UNSPECIFIED = 0
-    """Unspecified event type."""
-
-    CREATED = 1
-    """Event when a new resource is created."""
-
-    UPDATED = 2
-    """Event when an existing resource is updated."""
-
-    DELETED = 3
-    """Event when a resource is deleted."""
+__all__ = ["Event"]
