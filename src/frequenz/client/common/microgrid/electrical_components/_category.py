b58f272b29aec0567e1c62ab4cecda5a097f6f93
Leandro Lucarella <luca-frequenz@llucax.com>
2026-06-09 13:27:14 +0000

    Remove deprecated electrical component methods
    The explicit proto.v1alpha8 conversion helpers are the supported API.  Remove the deprecated enum instance conversion methods and the tests that covered those compatibility shims.
    
    Signed-off-by: Leandro Lucarella <luca-frequenz@llucax.com>

--- src/frequenz/client/common/microgrid/electrical_components/_category.py
+++ src/frequenz/client/common/microgrid/electrical_components/_category.py
@@ -3,17 +3,7 @@
 
 """Electrical component categories."""
 
-from __future__ import annotations
-
 import enum
-
-# pylint: disable=no-name-in-module
-from frequenz.api.common.v1alpha8.microgrid.electrical_components.electrical_components_pb2 import (
-    ElectricalComponentCategory as PBElectricalComponentCategory,
-)
-from typing_extensions import deprecated
-
-# pylint: enable=no-name-in-module
 
 
 @enum.unique
@@ -83,39 +73,3 @@ class ElectricalComponentCategory(enum.Enum):
 
     STEAM_BOILER = 19
     """A steam boiler, used to generate steam for heating or industrial processes."""
-
-    @classmethod
-    @deprecated(
-        "frequenz.client.common.microgrid.electrical_components."
-        "ElectricalComponentCategory.from_proto() is deprecated. "
-        "Use frequenz.client.common.microgrid.electrical_components.proto."
-        "v1alpha8.electrical_component_category_from_proto instead."
-    )
-    def from_proto(
-        cls, component_category: PBElectricalComponentCategory.ValueType
-    ) -> ElectricalComponentCategory:
-        """Convert a protobuf ElectricalComponentCategory message to enum.
-
-        Args:
-            component_category: protobuf enum to convert
-
-        Returns:
-            Enum value corresponding to the protobuf message.
-        """
-        if not any(t.value == component_category for t in ElectricalComponentCategory):
-            return ElectricalComponentCategory.UNSPECIFIED
-        return cls(component_category)
-
-    @deprecated(
-        "frequenz.client.common.microgrid.electrical_components."
-        "ElectricalComponentCategory.to_proto() is deprecated. "
-        "Use frequenz.client.common.microgrid.electrical_components.proto."
-        "v1alpha8.electrical_component_category_to_proto instead."
-    )
-    def to_proto(self) -> PBElectricalComponentCategory.ValueType:
-        """Convert a ElectricalComponentCategory enum to protobuf message.
-
-        Returns:
-            Enum value corresponding to the protobuf message.
-        """
-        return PBElectricalComponentCategory.ValueType(self.value)
--- src/frequenz/client/common/microgrid/electrical_components/_diagnostic_code.py
+++ src/frequenz/client/common/microgrid/electrical_components/_diagnostic_code.py
@@ -3,17 +3,7 @@
 
 """Electrical component diagnostic codes."""
 
-from __future__ import annotations
-
 import enum
-
-# pylint: disable=no-name-in-module
-from frequenz.api.common.v1alpha8.microgrid.electrical_components.electrical_components_pb2 import (
-    ElectricalComponentDiagnosticCode as PBElectricalComponentDiagnosticCode,
-)
-from typing_extensions import deprecated
-
-# pylint: enable=no-name-in-module
 
 
 @enum.unique
@@ -187,41 +177,3 @@ class ElectricalComponentDiagnosticCode(enum.Enum):
 
     INVERTER_DC_OVERVOLTAGE = 81
     """The inverter DC bus voltage is over the maximum rated value."""
-
-    @classmethod
-    @deprecated(
-        "frequenz.client.common.microgrid.electrical_components."
-        "ElectricalComponentDiagnosticCode.from_proto() is deprecated. "
-        "Use frequenz.client.common.microgrid.electrical_components.proto."
-        "v1alpha8.electrical_component_diagnostic_code_from_proto instead."
-    )
-    def from_proto(
-        cls, component_error_code: PBElectricalComponentDiagnosticCode.ValueType
-    ) -> ElectricalComponentDiagnosticCode:
-        """Convert a protobuf ElectricalComponentDiagnosticCode message to enum.
-
-        Args:
-            component_error_code: protobuf enum to convert
-
-        Returns:
-            Enum value corresponding to the protobuf message.
-        """
-        if not any(
-            c.value == component_error_code for c in ElectricalComponentDiagnosticCode
-        ):
-            return ElectricalComponentDiagnosticCode.UNSPECIFIED
-        return cls(component_error_code)
-
-    @deprecated(
-        "frequenz.client.common.microgrid.electrical_components."
-        "ElectricalComponentDiagnosticCode.to_proto() is deprecated. "
-        "Use frequenz.client.common.microgrid.electrical_components.proto."
-        "v1alpha8.electrical_component_diagnostic_code_to_proto instead."
-    )
-    def to_proto(self) -> PBElectricalComponentDiagnosticCode.ValueType:
-        """Convert a ElectricalComponentDiagnosticCode enum to protobuf message.
-
-        Returns:
-            Enum value corresponding to the protobuf message.
-        """
-        return PBElectricalComponentDiagnosticCode.ValueType(self.value)
