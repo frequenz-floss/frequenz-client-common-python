53f7317e438d231e147d9df3a65cc6a8c0ca4ea8
Leandro Lucarella <luca-frequenz@llucax.com>
2026-06-11 08:46:29 +0000

    Rename problematic component classes
    Signed-off-by: Leandro Lucarella <luca-frequenz@llucax.com>

--- src/frequenz/client/common/microgrid/electrical_components/_problematic.py
+++ src/frequenz/client/common/microgrid/electrical_components/_problematic.py
@@ -1,18 +1,18 @@
 # License: MIT
 # Copyright © 2024 Frequenz Energy-as-a-Service GmbH
 
-"""Unknown component."""
+"""Unknown electrical component."""
 
 import dataclasses
 from typing import Any, Literal, Self
 
 from ._category import ComponentCategory
-from ._component import Component
+from ._electrical_component import ElectricalComponent
 
 
 @dataclasses.dataclass(frozen=True, kw_only=True)
-class ProblematicComponent(Component):
-    """An abstract component with a problem."""
+class ProblematicComponent(ElectricalComponent):
+    """An abstract electrical component with a problem."""
 
     # pylint: disable-next=unused-argument
     def __new__(cls, *args: Any, **kwargs: Any) -> Self:
@@ -24,27 +24,27 @@ class ProblematicComponent(Component):
 
 @dataclasses.dataclass(frozen=True, kw_only=True)
 class UnspecifiedComponent(ProblematicComponent):
-    """A component of unspecified type."""
+    """An electrical component of unspecified type."""
 
     category: Literal[ComponentCategory.UNSPECIFIED] = ComponentCategory.UNSPECIFIED
-    """The category of this component."""
+    """The category of this electrical component."""
 
 
 @dataclasses.dataclass(frozen=True, kw_only=True)
 class UnrecognizedComponent(ProblematicComponent):
-    """A component of an unrecognized type."""
+    """An electrical component of an unrecognized type."""
 
     category: int
-    """The category of this component."""
+    """The category of this electrical component."""
 
 
 @dataclasses.dataclass(frozen=True, kw_only=True)
 class MismatchedCategoryComponent(ProblematicComponent):
-    """A component with a mismatch in the category.
+    """An electrical component with a mismatch in the category.
 
-    This component declared a category but carries category specific metadata that
-    doesn't match the declared category.
+    This electrical component declared a category but carries category specific
+    metadata that doesn't match the declared category.
     """
 
     category: ComponentCategory | int
-    """The category of this component."""
+    """The category of this electrical component."""
