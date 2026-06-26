73411c0aa85c397dc67b7fd3bb63f113bf2d2d9e
Leandro Lucarella <luca-frequenz@llucax.com>
2026-06-26 13:05:19 +0000

    Deprecate `Event.UNSPECIFIED`
    Mark `Event.UNSPECIFIED` as deprecated.
    
    Signed-off-by: Leandro Lucarella <luca-frequenz@llucax.com>

--- tests/streaming/test_event.py
+++ tests/streaming/test_event.py
@@ -3,13 +3,16 @@
 
 """Tests for the `Event` enum domain model."""
 
+import pytest
+
 from frequenz.client.common.streaming import Event
 
 
 def test_event_members() -> None:
     """Test that Event has the expected members with correct values."""
     assert [m.name for m in Event] == ["UNSPECIFIED", "CREATED", "UPDATED", "DELETED"]
-    assert Event.UNSPECIFIED.value == 0
+    with pytest.warns(DeprecationWarning):
+        assert Event.UNSPECIFIED.value == 0
     assert Event.CREATED.value == 1
     assert Event.UPDATED.value == 2
     assert Event.DELETED.value == 3
